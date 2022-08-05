e94e5c10d3fd2fcd0303f3f1b902f5ddff3dc6b3
QunFan <allenfan1012@gmail.com>
2022-08-06 06:42:10 +1000

    commit tree 39effe09a986c8e5672a1131f3f4f5413bfabd34

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const drivers = [];
+function findMatching(drivers, name) {
+    return drivers.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === name.toLowerCase()
+    )
+  }
+
+  function fuzzyMatch(drivers, name) {
+    return drivers.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(name.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter( dataCheck => dataCheck.name === name);
+  }
